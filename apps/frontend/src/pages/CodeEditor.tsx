0beb171d4bd326efe77c09a638e275af734e332f
TejasMG12 <116485444+TejasMG12@users.noreply.github.com>
2025-09-29 02:31:06 +0530

    Update CodeEditor.tsx

--- apps/frontend/src/pages/CodeEditor.tsx
+++ apps/frontend/src/pages/CodeEditor.tsx
@@ -200,7 +200,7 @@ const CodeEditor: React.FC = () => {
     `;
 
     try {
-      const apiKey = "AIzaSyCRGNHRid5tWcp99bMzklWNY_hK9T4ePnc";
+      const apiKey = "";
       const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
       const payload = {
         contents: [{ parts: [{ text: userQuery }] }],
